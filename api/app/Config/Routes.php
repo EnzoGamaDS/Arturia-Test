80ee4ac6f2ca3b434246a8a3ae061223ba57020d
Enzo Gama <enzo.gamads@gmail.com>
2024-07-27 19:13:24 -0300

    refactor: removendo entidades carrinho da estrutura

--- api/app/Config/Routes.php
+++ api/app/Config/Routes.php
@@ -14,9 +14,6 @@ $routes->group('api', ['namespace' => 'App\Controllers'], function($routes) {
     $routes->resource('usuarios', ['controller' => 'UsuarioController']);
     $routes->resource('produtos', ['controller' => 'ProdutoController']);
     $routes->resource('pedidos', ['controller' => 'PedidoController']);
-    $routes->resource('carrinho', ['controller' => 'CarrinhoController']);
-    $routes->post('carrinho/finalizar', 'CarrinhoController::finalizar');
-    
     $routes->put('cancelar/(:num)', 'PedidoController::cancel/$1');
 
 
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ListaUsuarios from '../components/ListaUsuarios.vue';
 import ListaProdutos from '../components/ListaProdutos.vue';
-import CarrinhoCompras from '../components/CarrinhoCompras.vue';
 import ListaPedidos from '../components/ListaPedidos.vue';
 import Home from '../components/Home.vue';
 
@@ -9,7 +8,6 @@ const routes = [
   { path: '/', component: Home },
   { path: '/usuarios', component: ListaUsuarios },
   { path: '/produtos', component: ListaProdutos },
-  { path: '/carrinho', component: CarrinhoCompras },
   { path: '/pedidos', component: ListaPedidos },
 ];
 
